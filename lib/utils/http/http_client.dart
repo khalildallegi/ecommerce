619570137768da939f0f32a89383aa4960d00fa0
khalildallegi <khalildallegi1@gmail.com>
2024-10-16 22:02:16 +0000

    m

---
+++ lib/utils/http/http_client.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class tHttpHelper {
+  static const String _baseUrl = "";
+  static Future<Map<String, dynamic>> get(String endpoint) async {
+    final Response = await http.get(Uri.parse('$_baseUrl/$endpoint'));
+    return _handleResponse(Response);
+  }
+
+  static Future<Map<String, dynamic>> post(
+      String endpoint, dynamic data) async {
+    final Response = await http.post(
+      Uri.parse('$_baseUrl/$endpoint'),
+      headers: {'content-type': 'application/json'},
+      body: json.encode(data),
+    );
+    return _handleResponse(Response);
+  }
+
+  static Future<Map<String, dynamic>> put(String endpoint, dynamic data) async {
+    final Response = await http.put(
+      Uri.parse('$_baseUrl /$endpoint'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(data),
+    );
+    return _handleResponse(Response);
+  }
+
+  static Future<Map<String, dynamic>> delete(String endpoint) async {
+    final Response = await http.delete(Uri.parse('$_baseUrl/$endpoint'));
+    return _handleResponse(Response);
+  }
+
+  static Map<String, dynamic> _handleResponse(http.Response response) {
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load data${response.statusCode}');
+    }
+  }
+}
